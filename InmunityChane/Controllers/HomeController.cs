257b2bb29d5fa9f6cd122be34e79992d148437c9
Monarca117 <skyggemonarken@gmail.com>
2024-02-03 19:45:33 -0600

    Actualizacion 2

--- InmunityChane/Controllers/HomeController.cs
+++ InmunityChane/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace InmunityChane.Controllers
             return View();
         }
 
+        public IActionResult User()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
